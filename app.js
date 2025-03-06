ccde0890c2e86d9ea7b8d7b2bc8dd2b210eb143b
ViveirosCathe <catherinedeviveiros@gmail.com>
2025-03-06 10:20:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+let listaDeNomesDosAmigos = [];
+let numeroLimiteDeNomes = 10 ;
+let amigoSecreto = "";
+let campoRecebeNome = "";
+let nomeAtual = "";
+let numeroDeAmigosAdicionados = 0 ;
+
+exibirMensagemInicial();
+//Função que automatiza a exibição de textos nos elementos do html.
+function exibirTextoNaTela(tag,texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+   
+ }
+
+//Função que chama a mensagem inicial usando a função exibirTextoNaTela.
+   function exibirMensagemInicial(){
+    exibirTextoNaTela("h2", "Digite o nome dos seus amigos");
+}
+// Função que adiciona a lista de nomes de amigos a cada clique do botão,
+//usando if-else para checar a condição de número máximo de amigoas na lista e 
+//se determinado nome já não foi adicionado antes, passando por essas duas condições
+//o nome dos amigos e adicionado ao vetor listaDeNomeDOsAmigos
+
+function adicionarAmigo(){
+    nomeAtual = document.querySelector("input").value;
+    campoRecebeNome = nomeAtual;
+    numeroDeAmigosAdicionados++;
+    
+    if (numeroDeAmigosAdicionados == numeroLimiteDeNomes + 1){
+        exibirTextoNaTela("h2","Você já adicionou o número máximo de amigos!");
+        limpaCampo();       
+        
+    } else{
+    
+      if (listaDeNomesDosAmigos.includes(nomeAtual)){
+        exibirTextoNaTela("h2","Este nome já foi adicionado!");         
+        limpaCampo();
+      }
+       else {
+        exibirTextoNaTela("h2", "");
+        listaDeNomesDosAmigos.push(campoRecebeNome);
+        limpaCampo();
+       }
+
+    }
+}
+//Função que desabilita o botão adicionar e o botão sortear.
+
+ function desabilitaInputEBotoes(){
+
+    document.getElementById("adicionar-amigo").disabled = true;
+    document.getElementById("sortearAmigo").disabled = true;
+ }
+
+ //Funçãoão que sorteia o nome de um amigo usando a funão Math.random para escolher um
+ //número aleatório e multiplica-lo pela quantidade de amigos adicionados e assim 
+ // o número obtido é usado para escolher o id de um nome no vetor dessa forma o amigo
+ // secreto é sorteado e exibido na tela.
+
+ function sortearAmigo(){
+  if (numeroDeAmigosAdicionados == 1){
+    exibirTextoNaTela("h2","Você precisa adicionar pelo menos 2 amigos para o sorteio!");
+  
+  } else {
+   
+    let idDoNomeSorteado = parseInt(Math.random() * numeroDeAmigosAdicionados);
+    amigoSecreto = listaDeNomesDosAmigos[idDoNomeSorteado];
+    exibirTextoNaTela("h2", "Seu amigo secreto é " + amigoSecreto);
+    document.getElementById("amigo").placeholder = "Aperte F5 para reiniciar o jogo";
+    desabilitaInputEBotoes();
+  }
+ }
+//Função que automatiza a limpeza do campo input.
+
+function limpaCampo(){
+    campoRecebeNome = document.querySelector("input");
+    campoRecebeNome.value = "";
+    
+  }
+
